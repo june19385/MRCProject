c7679324c4ad8c0cfb6c92258fc74e2c5f755630
Jeongeonwoo <ktfno18@ajou.ac.kr>
2018-07-19 12:37:34 +0900

    product_2

--- MRCProject/src/main/java/com/mrc/myapp/SubController.java
+++ MRCProject/src/main/java/com/mrc/myapp/SubController.java
@@ -9,10 +9,34 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class SubController {
-	@RequestMapping(value = "/products/product", method = RequestMethod.GET)
-	public String product(Locale locale, Model model) {
+	@RequestMapping(value = "/products/product1", method = RequestMethod.GET)
+	public String product1(Locale locale, Model model) {
 		
-		return "products/product";
+		return "products/product1";
+	}
+	
+	@RequestMapping(value = "/products/product2", method = RequestMethod.GET)
+	public String product2(Locale locale, Model model) {
+		
+		return "products/product2";
+	}
+	
+	@RequestMapping(value = "/products/product3", method = RequestMethod.GET)
+	public String product3(Locale locale, Model model) {
+		
+		return "products/product3";
+	}
+	
+	@RequestMapping(value = "/products/product4", method = RequestMethod.GET)
+	public String product4(Locale locale, Model model) {
+		
+		return "products/product4";
+	}
+	
+	@RequestMapping(value = "/products/product5", method = RequestMethod.GET)
+	public String product5(Locale locale, Model model) {
+		
+		return "products/product5";
 	}
 
 }
